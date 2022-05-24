23a02a84a59c643b66fdfe8f21a7ac1f987815b6
Pranjal Sahu <pranjalsahu5@gmail.com>
2022-05-24 13:35:35 -0400

    ENH: Working code with cmake 5

---
+++ itk-module.cmake
@@ -0,0 +1,23 @@
+# the top-level README is used for describing this module, just
+# re-used it for documentation here
+get_filename_component(MY_CURRENT_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+file(READ "${MY_CURRENT_DIR}/README.md" DOCUMENTATION)
+
+itk_module(Ransac
+  ENABLE_SHARED
+  COMPILE_DEPENDS
+    ITKCommon
+    ITKMesh
+    ITKIOMeshBase
+    ITKRegistrationCommon
+    ITKMetricsv4
+    ITKRegistrationMethodsv4
+    ITKOptimizersv4
+    ITKOptimizers
+  TEST_DEPENDS
+    ITKTestKernel
+    ITKMetaIO
+  DESCRIPTION
+    "${DOCUMENTATION}"
+  EXCLUDE_FROM_DEFAULT
+)
