2f6b948d1783b85aa87e4187aee73df004b09ed2
Pranjal Sahu <pranjalsahu5@gmail.com>
2022-08-25 19:19:46 -0400

    Some optimization

--- examples/testregistration.py
+++ examples/testregistration.py
@@ -3,10 +3,12 @@ import numpy as np
 import random
 import copy
 
+casename = 'GJCIEB'
+
 def GenerateData(data, agreeData):
     dimension = 6
-    mesh1 = itk.meshread("/data/Apedata/Slicer-cli-outputs/EJEJHH_moving_corr.vtk", itk.F)
-    mesh2 = itk.meshread("/data/Apedata/Slicer-cli-outputs/EJEJHH_fixed_corr.vtk", itk.F)
+    mesh1 = itk.meshread("/data/Apedata/Slicer-cli-outputs/"+casename+"_moving_corr.vtk", itk.F)
+    mesh2 = itk.meshread("/data/Apedata/Slicer-cli-outputs/"+casename+"_fixed_corr.vtk", itk.F)
     data.reserve(mesh1.GetNumberOfPoints())
     for i in range(mesh1.GetNumberOfPoints()):
         point1 = mesh1.GetPoint(i)
@@ -15,8 +17,8 @@ def GenerateData(data, agreeData):
         input_data = [float(x) for x in input_data]
         data.push_back(input_data)
     
-    mesh1 = itk.meshread("/data/Apedata/Slicer-cli-outputs/EJEJHH_movingMeshPointsBefore.vtk", itk.F)
-    mesh2 = itk.meshread("/data/Apedata/Slicer-cli-outputs/EJEJHH_fixedMeshPoints.vtk", itk.F)
+    mesh1 = itk.meshread("/data/Apedata/Slicer-cli-outputs/"+casename+"_movingMeshPointsBefore.vtk", itk.F)
+    mesh2 = itk.meshread("/data/Apedata/Slicer-cli-outputs/"+casename+"_fixedMeshPoints.vtk", itk.F)
     count_min = int(np.min([mesh1.GetNumberOfPoints(), mesh2.GetNumberOfPoints()]))
 
     mesh1_points  = copy.deepcopy(itk.array_from_vector_container(mesh1.GetPoints()))
@@ -44,12 +46,12 @@ outlierDistance = 20.0
 RANSACType = itk.RANSAC[itk.Point[itk.D, 6], itk.D]
 
 transformParameters = itk.vector.D()
-maximalDistanceFromPlane = 1.7
+maximumDistance = 3
 RegistrationEstimatorType = itk.Ransac.LandmarkRegistrationEstimator[6]
 registrationEstimator = RegistrationEstimatorType.New()
 registrationEstimator.SetMinimalForEstimate(100)
 registrationEstimator.SetAgreeData(agreeData)
-registrationEstimator.SetDelta(maximalDistanceFromPlane)
+registrationEstimator.SetDelta(maximumDistance)
 registrationEstimator.LeastSquaresEstimate(data, transformParameters)
 
 print("Least squares hyper(plane) parameters: [n,a]")
@@ -61,7 +63,7 @@ ransacEstimator = RANSACType.New()
 ransacEstimator.SetData(data)
 ransacEstimator.SetAgreeData(agreeData)
 ransacEstimator.SetMaxIteration(1000)
-ransacEstimator.SetNumberOfThreads(8)
+ransacEstimator.SetNumberOfThreads(1)
 ransacEstimator.SetParametersEstimator(registrationEstimator)
 percentageOfDataUsed = ransacEstimator.Compute( transformParameters, desiredProbabilityForNoOutliers )
 
@@ -81,8 +83,8 @@ for i in range(7, 10):
 transform.SetParameters(p)
 transform.SetFixedParameters(f)
 
-mesh1 = itk.meshread("/data/Apedata/Slicer-cli-outputs/EJEJHH_movingMesh.vtk", itk.F)
+mesh1 = itk.meshread("/data/Apedata/Slicer-cli-outputs/"+casename+"_movingMesh.vtk", itk.F)
 output = itk.transform_mesh_filter(mesh1, transform=transform)
-itk.meshwrite(output, '/data/Apedata/Slicer-cli-outputs/EJEJHH_movingMesh_transformed.vtk')
+itk.meshwrite(output, "/data/Apedata/Slicer-cli-outputs/"+casename+"_movingMesh_transformed.vtk")
 
 
