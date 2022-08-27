f13d1b67f096f24894ec5fa7a72a2b000f9807c2
Pranjal Sahu <pranjalsahu5@gmail.com>
2022-08-26 21:41:54 -0400

    Fix for windows error

--- include/itkLandmarkRegistrationEstimator.h
+++ include/itkLandmarkRegistrationEstimator.h
@@ -29,7 +29,7 @@ namespace itk
 {
 
 template <unsigned int Dimension>
-class ITK_TEMPLATE_EXPORT LandmarkRegistrationEstimator : public ParametersEstimator<Point<double, Dimension>, double>
+class ITK_TEMPLATE_EXPORT LandmarkRegistrationEstimator : public itk::ParametersEstimator<Point<double, Dimension>, double>
 {
 public:
   typedef LandmarkRegistrationEstimator                         Self;
