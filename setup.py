b1c5e66f314e8d27081bdab98217cfa5ada120be
Pranjal Sahu <pranjal.sahu@kitware.com>
2022-11-14 17:58:40 -0500

    Update URL and description in setup.py

--- setup.py
+++ setup.py
@@ -17,9 +17,9 @@ setup(
     author_email='pranjalsahu5@gmail.com',
     packages=['itk'],
     package_dir={'itk': 'itk'},
-    download_url=r'https://github.com/PranjalSahu/ITKRANSAC.git',
-    description=r'This is a template that serves as a starting point for a new module.',
-    long_description='ITK is an open-source, cross-platform library that provides developers with an extensive suite of software tools for image analysis. Developed through extreme programming methodologies, ITK employs leading-edge algorithms for registering and segmenting multidimensional scientific images.',
+    download_url=r'https://github.com/InsightSoftwareConsortium/ITKRANSAC',
+    description=r'An ITK-based implementation of RANSAC used for point cloud registration.',
+    long_description='It supports feature based registration and can be used along with the FPFH remote module. The class itkRANSAC is the main driver that takes an object of class itkLandmarkRegistrationEstimator as argument. Please refer to the documentation for a detailed description and sample usage: https://github.com/InsightSoftwareConsortium/ITKRANSAC.',
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
