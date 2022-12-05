6b0417df522b288536a5bbcf1dafd007b5314c59
Pranjal Sahu <pranjalsahu5@gmail.com>
2022-12-05 14:18:32 -0500

    Bump tag to 0.1.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
 
 setup(
     name='itk-ransac',
-    version='0.1.0',
+    version='0.1.1',
     author='Pranjal Sahu',
     author_email='pranjalsahu5@gmail.com',
     packages=['itk'],
